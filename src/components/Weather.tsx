7dc3eb86ca81aee614d23c6bc85157c2e2e56002
Asad <hellohello51981@gmail.com>
2025-08-23 12:39:11 +0500

    feat: Add the JS

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import { faSearch,faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const Weather:React.FC = () => {
   return (
     <div className='w-full min-w-112 bg-bg-black-2 rounded-2xl h-[45%]'>
-      Weather
+      <div>
+        <div>
+          <FontAwesomeIcon icon={faSearch}/>
+          <div>Sialkot</div>
+        </div>
+        <div>
+          <input type="text" placeholder="Enter Location"/>
+          <FontAwesomeIcon icon={faMagnifyingGlass}/>
+        </div>
+      </div>
+      <div>
+        <i class='bxr  bx-sun'  ></i>
+        <div>Clear</div>
+        <div>29</div>
+      </div>
     </div>
   )
 }
