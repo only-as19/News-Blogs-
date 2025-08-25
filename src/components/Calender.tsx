35141e1e74a4013ba11f82fc526341bf33d2cc1f
Asad <hellohello51981@gmail.com>
2025-08-25 12:54:27 +0500

    Style the Calender

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -2,57 +2,73 @@ import React from "react";
 
 const Calender: React.FC = () => {
   return (
-    <div>
-      <div >
-        <h2>August</h2>
-        <h2>2025</h2>
-        <div>
-          <div >
+    <div className="w-full min-w-112 bg-bg-black-2 rounded-2xl h-[calc(55%-2rem)] p-6 grid place-items-center">
+      <div className="w-full flex justify-between items-center pl-4 mb-4 gap-x-4">
+        <h2 className="text-3xl font-bold text-highlight">August</h2>
+        <h2 className="text-3xl font-bold text-highlight">2025</h2>
+        <div className="flex ml-auto gap-x-4">
+          <div className="w-14 h-14 flex justify-center items-center cursor-pointer text-3xl bg-[#2c3542] rounded-[50%]">
             <i className="bx bx-chevron-left text-[#b88efc]"></i>
           </div>
-          <div >
+          <div className="w-14 h-14 flex justify-center items-center cursor-pointer text-3xl bg-[#2c3542] rounded-[50%]">
             <i className="bx bx-chevron-right text-[#b88efc]"></i>
           </div>
         </div>
       </div>
-      <div >
-        <span >
+      <div className="flex w-full">
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
           Sun
         </span>
-        <span >
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
           Mon
         </span>
-        <span >
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
           Tue
         </span>
-        <span >
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
           Wed
         </span>
-        <span >
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
           Thu
         </span>
-        <span >9</span>
-        <span>10</span>
-        <span>11</span>
-        <span>12</span>
-        <span>13</span>
-        <span>14</span>
-        <span>15</span>
-        <span>16</span>
-        <span>17</span>
-        <span>18</span>
-        <span>19</span>
-        <span>20</span>
-        <span>21</span>
-        <span>22</span>
-        <span>23</span>
-        <span>24</span>
-        <span>25</span>
-        <span>26</span>
-        <span>27</span>
-        <span>28</span>
-        <span>29</span>
-        <span>30</span>
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
+          Fri
+        </span>
+        <span className="w-[calc(100%/7)] text-xl text-zinc-500 uppercase flex justify-center">
+          Sat
+        </span>
+      </div>
+      <div className="flex flex-wrap">
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">1</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">2</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">3</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">4</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">5</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">6</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">7</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">8</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center cursor-pointer">9</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>10</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>11</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>12</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>13</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>14</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>15</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>16</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>17</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>18</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>19</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>20</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>21</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>22</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>23</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>24</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>25</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>26</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>27</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>28</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>29</span>
+        <span className=" w-[calc(100%/7)] text-xl text-zinc-500 flex justify-center aspect-1/1 items-center" cursor-pointer>30</span>
       </div>
     </div>
   );
