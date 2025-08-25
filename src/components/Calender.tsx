ef56ff50e8c04e00911943a9bd5a1b6b5a42e0a9
Asad <hellohello51981@gmail.com>
2025-08-25 12:53:16 +0500

    Add the jsx

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
 
-const Calender:React.FC = () => {
+const Calender: React.FC = () => {
   return (
-    <div className='w-full min-w-112 bg-bg-black-2 rounded-2xl h-[calc(55%-2rem)]'>
-      Calender
+    <div>
+      <div >
+        <h2>August</h2>
+        <h2>2025</h2>
+        <div>
+          <div >
+            <i className="bx bx-chevron-left text-[#b88efc]"></i>
+          </div>
+          <div >
+            <i className="bx bx-chevron-right text-[#b88efc]"></i>
+          </div>
+        </div>
+      </div>
+      <div >
+        <span >
+          Sun
+        </span>
+        <span >
+          Mon
+        </span>
+        <span >
+          Tue
+        </span>
+        <span >
+          Wed
+        </span>
+        <span >
+          Thu
+        </span>
+        <span >9</span>
+        <span>10</span>
+        <span>11</span>
+        <span>12</span>
+        <span>13</span>
+        <span>14</span>
+        <span>15</span>
+        <span>16</span>
+        <span>17</span>
+        <span>18</span>
+        <span>19</span>
+        <span>20</span>
+        <span>21</span>
+        <span>22</span>
+        <span>23</span>
+        <span>24</span>
+        <span>25</span>
+        <span>26</span>
+        <span>27</span>
+        <span>28</span>
+        <span>29</span>
+        <span>30</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calender
+export default Calender;
