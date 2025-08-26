2f0136f30af9df547acc9c2bd894b26da638e6d2
Asad <hellohello51981@gmail.com>
2025-08-26 12:27:49 +0500

    feat: Add the jsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { News , Blogs } from "./components"
 const App:React.FC  = ()=> {
   return (
     <div className="main-bg w-full h-screen grid place-items-center text-white">
-      <div className=" w-[95vw] h-[95vmin] bg-main-bg rounded-md shadows">
+      <div className=" w-[95vw] h-[95vmin] bg-main-bg rounded-xl shadows">
         {/* <News/> */}
         <Blogs/>
       </div>
--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import userImage from "../assets/images/user.jpg";
+import bgImage from "../assets/images/bg.jpg";
 const Blogs = () => {
   return (
     <div>
-      
+      <div
+       
+        style={{ backgroundImage: `url('${bgImage}')` }}
+      >
+        <img
+          src={userImage}
+          alt="Asad Latif"
+          
+        />
+      </div>
+      <div >
+        <button >Create a new Post</button>
+        <button >
+          <span>Back</span>
+          <span >
+            <i className="bx bx-chevron-right"></i>
+          </span>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
